7b022527158f66b4838c7d1c87e7f8494f2bd31c
lelotz <olomidereck@hotmail.com>
2023-03-31 11:40:25 +0300

    added checkIn testing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,27 +5,29 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
+
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hotel_pms/app/data/models_n/session_tracker.dart';
+
+import 'package:hotel_pms/app/modules/check_in_screen/tests/check_in_unit_tests.dart';
 import 'package:hotel_pms/app/modules/login_screen/tests/auth_controller_unit_tests.dart';
 import 'package:hotel_pms/app/modules/login_screen/tests/session_manager_unit_test.dart';
-import 'package:hotel_pms/core/logs/logger_instance.dart';
-import 'package:hotel_pms/core/session_management/session_manager.dart';
-
-import 'package:hotel_pms/main.dart';
-import 'package:hotel_pms/widgets/text/big_text.dart';
+import 'package:hotel_pms/mock_data/mock_data_api.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
-class TestObject{
-  String? hello;
-  TestObject({required this.hello});
 
-  set setHello (String str){str=hello!;}
-}
 void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
   sqfliteFfiInit();
-  sessionManagerUnitTest();
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+
+  //await SqlDatabase.instance.initiateDatabase();
+  //WidgetsFlutterBinding.ensureInitialized();
+
+
+  await loadMockNamesAndCountries();
+
+  await sessionManagerUnitTest();
   await authControllerUnitTest();
+  await checkInControllerUnitTests();
 
 }
